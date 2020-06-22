2164b046a28b9589a54e318e6fe39df9bc3c7053
Amar Singh <47265955+AMARsingh98@users.noreply.github.com>
2020-06-22 15:05:05 +0530

    Add files via upload

---
+++ resume-builder-master/app.js
@@ -0,0 +1,47 @@
+new Vue({
+	el: '#vue-app',
+	data: {
+        name: 'Amar Singh Sengar',
+        designation:'Software Engineer',
+		address: '111 Lorem Street, 34785, Ipsum City',
+		phone: '+91 9799295681',
+        email: 'amarsinghsengar98@gmail.com',
+        date:'',
+        objective:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dignissim viverra nibh sed varius. Proin bibendum nunc in sem ultrices posuere. Aliquam ut aliquam lacus.',
+        sdate:'',
+        edate:'',
+        schoolname:'',
+        schoolocation:'',
+        schoolscore:'',
+        sdate1:'',
+        sdate1:'',
+        hschoolname:'',
+        hschoolocation:'',
+        hschoolscore:'',
+        sdate2:'',
+        edate2:'',
+        collegename:'',
+        collegelocation:'',
+        collegescore:'',
+        c:'',
+        java:'',
+        htmlcss:'',
+        js:'',
+        php:'',
+        db:'',
+        ach1:'',
+        ach2:'',
+        ach3:'',
+        companyname1:'',
+        comdesignation1:'',
+        sdate3:'',
+        edate3:'',
+        companylocation1:'',
+        cdes1:''
+        
+	},
+	methods: {
+		
+		
+	}
+});
